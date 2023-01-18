efb7822e2d8049f2feb3a9498d59477e763ccd5b
matthewcluck <matthewacluck@gmail.com>
2023-01-17 19:17:04 -0600

    Cleaned up central taskbox and added various event listeners

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const projects = document.querySelectorAll('.list');
+const initialActive = document.querySelector('.lista');
 const addProject = document.querySelector('.plus');
 const projectsPane = document.querySelector('#projects');
+const title = document.querySelector('#current');
+const done = document.querySelectorAll('#done');
+const priorities1 = document.querySelectorAll('.priority1');
+const priorities2 = document.querySelectorAll('.priority2');
+const priorities3 = document.querySelectorAll('.priority3');
+
+initialActive.addEventListener('dblclick', function(){
+    let input = prompt('Enter Project Name','House Cleaning');
+    initialActive.innerHTML = input;
+})
+
+initialActive.addEventListener('click', function(){
+    let active = document.querySelector('.lista');
+    title.innerHTML = initialActive.innerHTML;
+    active.classList.remove('lista');
+    active.classList.add('list');
+    initialActive.classList.add('lista');
+})
 
 addProject.addEventListener('click', function(){
     let newProj = document.createElement('div');
@@ -12,8 +31,16 @@ addProject.addEventListener('click', function(){
         newProj.remove();
     })
     newProj.addEventListener('dblclick', function(){
-        let input = prompt('Enter Project Name');
+        let input = prompt('Enter Project Name','New Project');
         newProj.innerHTML = (input);
+    })
+    newProj.addEventListener('click', function(){
+        let active = document.querySelector('.lista');
+        console.log(active);
+        title.innerHTML = newProj.innerHTML;
+        active.classList.remove('lista');
+        active.classList.add('list');
+        newProj.classList.add('lista');
     })
 })
 
@@ -23,10 +50,35 @@ projects.forEach(project => {
         project.remove();
     })
     project.addEventListener('dblclick', function(){
-        let input = prompt('Enter Project Name');
+        let input = prompt('Enter Project Name',project.innerHTML);
         project.innerHTML = (input);
     })
+    project.addEventListener('click', function(){
+        let active = document.querySelector('.lista');
+        console.log(active);
+        title.innerHTML = project.innerHTML;
+        active.classList.remove('lista');
+        active.classList.add('list');
+        project.classList.add('lista');
+    })
 })
+
+function priorityFunc(e){
+    console.log(e);
+}
+
+priorities1.forEach(priority => {
+    priority.addEventListener('click', priorityFunc(priority.class))
+})
+
+
+// priorities1.forEach(priority => {
+//     priority.addEventListener('click', function(){
+//         priority.classList.remove('priority1');
+//         priority.classList.add('priority2');
+//         console.log('2');
+//     })
+// })
 
 class Task {
     constructor(name, priority, date) {
