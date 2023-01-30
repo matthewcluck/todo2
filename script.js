911d9522088cd84506c0322318eb48074bfc5b5b
matthewcluck <matthewacluck@gmail.com>
2023-01-29 18:43:13 -0600

    Changing project page added

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ const dones = document.querySelectorAll('.done');
 const dues = document.querySelectorAll('#due');
 const priorities = document.querySelectorAll('.priority');
 const plusTask = document.querySelector('#plustask');
-const taskBox = document.querySelector('#taskbox');
+const taskBox = document.querySelector('#taskbox1');
+const taskBox2 = document.querySelector('#taskbox2');
+const taskBox3 = document.querySelector('#taskbox3');
 const initialTasks = document.querySelectorAll('.clist');
+const taskboxParent = document.querySelector('#tasks')
+
+taskBox2.style.display = "none";
+taskBox3.style.display = "none";
 
 initialActive.addEventListener('dblclick', function(){
     let input = prompt('Enter Project Name','House Cleaning');
@@ -21,13 +27,84 @@ initialActive.addEventListener('click', function(){
     active.classList.remove('lista');
     active.classList.add('list');
     initialActive.classList.add('lista');
+    if (initialActive.id == '1'){
+        taskBox.style.display = "flex";
+        taskBox2.style.display = "none";
+        taskBox3.style.display = "none";
+    } else if (initialActive.id == '2'){
+        taskBox.style.display = "none";
+        taskBox2.style.display = "flex";
+        taskBox3.style.display = "none";
+    } else {
+        taskBox.style.display = "none";
+        taskBox2.style.display = "none";
+        taskBox3.style.display = "flex";
+    }
 })
+
+let i = 4;
 
 addProject.addEventListener('click', function(){
     let newProj = document.createElement('div');
     newProj.classList.add('list');
     let addProj = projectsPane.insertBefore(newProj,addProject);
     addProj.innerHTML = ('New Project');
+
+    let newTaskbox = document.createElement('div');
+    newTaskbox.classList.add('taskbox');
+    newTaskbox.id = `taskbox${i}`;
+    i++;
+
+    taskboxParent.appendChild(newTaskbox);
+
+    let taskHeader = document.createElement('div');
+    taskHeader.innerHTML = 'Task';
+    taskHeader.classList.add('header');
+    taskHeader.id = 'task';
+
+    let priorityHeader = document.createElement('div');
+    priorityHeader.innerHTML = 'Priority';
+    priorityHeader.classList.add('header');
+    priorityHeader.id = 'priority';
+
+    let dueHeader = document.createElement('div');
+    dueHeader.innerHTML = 'Due';
+    dueHeader.classList.add('header');
+    dueHeader.id = 'due';
+
+    let doneHeader = document.createElement('div');
+    doneHeader.innerHTML = 'Done';
+    doneHeader.classList.add('header');
+    doneHeader.id = 'done';
+
+    newTaskbox.appendChild(taskHeader);
+    newTaskbox.appendChild(priorityHeader);
+    newTaskbox.appendChild(dueHeader);
+    newTaskbox.appendChild(doneHeader);
+
+    let newClist = document.createElement('div');
+    newClist.classList.add('clist');
+    newClist.id = addProj.id;
+
+    let newPriority = document.createElement('div');
+    newPriority.classList.add('priority');
+    newPriority.id = addProj.id;
+
+    let newDue = document.createElement('div');
+    newDue.classList.add('due');
+    newDue.id = addProj.id;
+
+    let newDone = document.createElement('div');
+    newDone.classList.add('done');
+    newDone.id = addProj.id;
+
+    newTaskbox.appendChild(newClist);
+    newTaskbox.appendChild(newPriority);
+    newTaskbox.appendChild(newDue);
+    newTaskbox.appendChild(newDone);
+
+    newTaskbox.style.display = 'none';
+
     newProj.addEventListener('contextmenu', function(a){
         a.preventDefault();
         newProj.remove();
@@ -37,12 +114,22 @@ addProject.addEventListener('click', function(){
         newProj.innerHTML = (input);
     })
     newProj.addEventListener('click', function(){
+        let taskboxes = document.querySelectorAll('.taskbox');
+
         let active = document.querySelector('.lista');
-        console.log(active);
         title.innerHTML = newProj.innerHTML;
         active.classList.remove('lista');
         active.classList.add('list');
         newProj.classList.add('lista');
+
+        taskboxes.forEach(taskbox => {
+            if (taskbox.id == newTaskbox.id){
+                taskbox.style.display = 'flex';
+            } else {
+                taskbox.style.display = 'none';
+            }
+        })
+
     })
 })
 
@@ -57,12 +144,26 @@ projects.forEach(project => {
     })
     project.addEventListener('click', function(){
         let active = document.querySelector('.lista');
-        console.log(active);
         title.innerHTML = project.innerHTML;
         title.className = project.id;
         active.classList.remove('lista');
         active.classList.add('list');
         project.classList.add('lista');
+
+        if (project.id == '1'){
+            taskBox.style.display = "flex";
+            taskBox2.style.display = "none";
+            taskBox3.style.display = "none";
+        } else if (project.id == '2'){
+            taskBox.style.display = "none";
+            taskBox2.style.display = "flex";
+            taskBox3.style.display = "none";
+        } else {
+            taskBox.style.display = "none";
+            taskBox2.style.display = "none";
+            taskBox3.style.display = "flex";
+        }
+
     })
 })
 
